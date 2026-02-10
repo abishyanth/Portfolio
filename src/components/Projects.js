c1d9d47418c31e4fd04e00567d5214ae7d461a1f
ABISHYANTH.S <160244754+abishyanth@users.noreply.github.com>
2026-02-10 10:57:21 +0530

    initial comit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { motion,} from 'framer-motion';
 import { Github, ArrowRight } from 'lucide-react';
 import SpotlightCard from './ui/SpotlightCard';
 import './Projects.css';
